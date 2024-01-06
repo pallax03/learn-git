e7ebb5937a8035c7594c8c476a60cb6be39ecb7e
Samuele Casadei <samuele.casadei3@studio.unibo.it>
2024-01-06 10:58:08 +0100

    Now HelloWorld generates a random int after the phrase

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Random;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World\n");
+        System.out.println("Hello World "+new Random().nextInt()+"\n");
+
     }
+
 }
